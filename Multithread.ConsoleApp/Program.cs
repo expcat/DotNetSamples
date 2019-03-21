9a634624639401d529502667de2613d0045e69ae
Yizhe Wang <holysdking@hotmail.com>
2019-03-22 04:22:32 +0800

    Add Multithread Samples

---
+++ Multithread.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Multithread.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
